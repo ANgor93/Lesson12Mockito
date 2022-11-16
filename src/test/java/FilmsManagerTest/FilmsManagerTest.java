30578ee46266f1e560891acae7451f51c5542c10
Alexey Gorokhov <bring974@gmail.com>
2022-11-16 17:56:09 +0300

    second commit

---
+++ src/test/java/FilmsManagerTest/FilmsManagerTest.java
@@ -0,0 +1,63 @@
+package FilmsManagerTest;
+import FilmsManager.FilmsManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmsManagerTest {
+
+    @Test
+    public void addFilmTest() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Star Wars 1");
+        manager.addFilm("Star Wars 2");
+        manager.addFilm("Star Wars 3");
+        manager.addFilm("Star Wars 4");
+        manager.addFilm("Star Wars 5");
+        manager.addFilm("Star Wars 6");
+        manager.addFilm("Star Wars 7");
+        manager.addFilm("Star Wars 8");
+        manager.addFilm("Star Wars 9");
+        manager.addFilm("Star Wars 10");
+
+        String[] expected = {"Star Wars 1", "Star Wars 2", "Star Wars 3", "Star Wars 4", "Star Wars 5", "Star Wars 6", "Star Wars 7", "Star Wars 8", "Star Wars 9", "Star Wars 10"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastTest() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Star Wars 1");
+        manager.addFilm("Star Wars 2");
+        manager.addFilm("Star Wars 3");
+        manager.addFilm("Star Wars 4");
+        manager.addFilm("Star Wars 5");
+        manager.addFilm("Star Wars 6");
+        manager.addFilm("Star Wars 7");
+        manager.addFilm("Star Wars 8");
+        manager.addFilm("Star Wars 9");
+        manager.addFilm("Star Wars 10");
+
+        String[] expected = {"Star Wars 10", "Star Wars 9", "Star Wars 8", "Star Wars 7", "Star Wars 6", "Star Wars 5", "Star Wars 4", "Star Wars 3", "Star Wars 2", "Star Wars 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void testFindOverLimit() {
+        FilmsManager manager = new FilmsManager(15);
+
+        manager.addFilm("Star Wars 1");
+        manager.addFilm("Star Wars 2");
+        manager.addFilm("Star Wars 3");
+
+        String[] expected = {"Star Wars 3", "Star Wars 2", "Star Wars 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
